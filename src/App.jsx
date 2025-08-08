ecfd752f4dad2e79e3a260d1a9b08cc3c648daa0
nihatran <89045243+nihatran@users.noreply.github.com>
2025-08-07 20:10:17 -0400

    Set up files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>To Do List</h1>
+        <input type="text"></input>
+        <button className="addBtn">+</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
